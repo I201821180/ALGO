3a64a0ce899483cb16cecf0d77f9febc9fbc3966
pytlab <shaozhengjiang@gmail.com>
2016-11-27 14:08:17 +0800

    大数乘法

--- big_number_multiplication.cpp
+++ big_number_multiplication.cpp
@@ -1,5 +1,11 @@
+/*
+ * 大数乘法
+ */
 #include <iostream>
 #include <memory.h>
+
+#define SIZE1 10
+#define SIZE2 10
 
 int * multi(int * num1, int size1, int * num2, int size2)
 {
@@ -12,24 +18,44 @@ int * multi(int * num1, int size1, int * num2, int size2)
     // 遍历右侧加数的各位
     for (int i2 = 0; i2 < size2; ++i2)
     {
-        int k = i2; // 返回数组的索引
-        
+        int ri = size - i2 - 1;
+        int ri2 = size2 - i2 - 1;
         for (int i1 = 0; i1 < size1; ++i1)
         {
-            ret[k++] += num2[i2]*num1[i1];
+            int ri1 = size1 - i1 - 1;
+            ret[ri--] += num1[ri1]*num2[ri2];
         }
     }
 
     // 进位处理
-    for (int i = 0; i < size; ++i)
+    for (int i = size-1; i > 0; --i)
     {
         if (ret[i] >= 10)
         {
-            ret[i-1] += 1;
+            ret[i-1] += ret[i] / 10;
             ret[i] %= 10;
         }
     }
 
     return ret;
+}
+
+int main()
+{
+    int n1[SIZE1] = {1, 1, 1, 2, 3, 4, 5, 8, 2, 7};
+    int n2[SIZE2] = {2, 3, 1, 2, 3, 3, 6, 7, 6, 6};
+
+    int * ret = multi(n1, SIZE1, n2, SIZE2);
+
+    int size = SIZE1 + SIZE2;
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout.width(2);
+        std::cout << *(ret + i);
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
 
