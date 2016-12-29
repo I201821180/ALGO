26abce41e121b56712b324e2447766c3c73e0cbd
pytlab <shaozhengjiang@gmail.com>
2016-12-29 20:05:34 +0800

    模式匹配(MP算法)

--- pattern_match_MP.cpp
+++ pattern_match_MP.cpp
@@ -3,28 +3,23 @@
 #include <cstring>
 #include <iostream>
 
+/*
+ * 获取失效列表的过程也是一个模式匹配过程，需要是两个模式字符串进行相对平移来进行匹配。
+ */
 void calF(const char * s, int * F)
 {
-    F[0] = 0;
-    int i = 1, j = 0;
+    int i = 0;
+    int j = -1;
+    F[0] = -1;
 
     while (s[i] != '\0')
     {
-        if (s[i] == s[j])
+        while (j > -1 && s[i] != s[j])
         {
-            F[i++] = ++j;
+            // 利用前面的失效函数进行对齐
+            j = F[j];
         }
-        else
-        {
-            if (j > 0)
-            {
-                j = 0;
-            }
-            else
-            {
-                F[i++] = j;
-            }
-        }
+        F[++i] = ++j;
     }
 }
 
@@ -35,29 +30,24 @@ int MP_match(const char * target, const char * pattern)
 
     int * F = new int[len_pattern];
 
+    // 获取失效列表
+    calF(pattern, F);
+
     while (target[i] != '\0')
     {
-        if (target[i] == pattern[j])
+        while (j >= 0 && target[i] != pattern[j])
         {
-            i++;
-            j++;
+            // 平移模式串对齐
+            j = F[j];
+        }
 
-            if (j >= len_pattern)
-            {
-                return i - len_pattern;
-            }
-        }
-        else
+        if (j >= len_pattern - 1)
         {
-            if (j == 0)
-            {
-                i++;
-            }
-            else
-            {
-                j = F[i-1];
-            }
+            return i - len_pattern + 1;
         }
+
+        j++;
+        i++;
     }
 
     delete [] F;
